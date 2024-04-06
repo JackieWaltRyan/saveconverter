5ef35b10531cdbab0c4e0a3e53334fde3b2a492d
Jackie Ryan <jackiewaltryan@gmail.com>
2024-04-06 20:25:58 +0300

    Add files via upload

--- SAVEconverter.py
+++ SAVEconverter.py
@@ -4,35 +4,35 @@ from re import findall, sub, MULTILINE
 from sys import exit, argv
 
 
-def find_bin_files(keywords, files, folder=""):
+def find_bin_files(files, folder=""):
     try:
         response, i, ii = True, 1, 1
-        
+
         for file in files:
             print(f"[{i} из {len(files)}] Ищем данные в: {folder}{file}")
-            
+
             try:
                 data = b""
-                
+
                 with open(file=f"{folder}{file}",
                           mode="rb") as input_bin_file:
                     trigger = False
-                              
+
                     for line in input_bin_file.readlines():
                         if findall(pattern=b"<MLP_Save",
                                    string=line):
                             trigger = True
-                            
+
                         if trigger:
                             data += line
-                            
+
                         if findall(pattern=b"MLP_Save>",
                                    string=line):
                             trigger = False
-                        
-                if (len(data) >= 100000):
+
+                if len(data) >= 100000:
                     print("    Данные найдены. Обработка данных.")
-                        
+
                     data = sub(pattern=rb'^.*<MLP_Save',
                                repl=b'<MLP_Save',
                                string=data)
@@ -70,94 +70,93 @@ def find_bin_files(keywords, files, folder=""):
                                repl=rb'<\1/>',
                                string=data,
                                flags=MULTILINE)
-                    data = sub(pattern=rb'[]',
+                    data = sub(pattern=rb'''[]''',
                                repl=b'',
                                string=data)
                     data = sub(pattern=rb' \'',
                                repl=b'=',
                                string=data)
-                    
+
                     if not exists(path="SAVEconverter"):
                         print(f"    Создание папки SAVEconverter.\n")
-            
+
                         try:
                             makedirs(name="SAVEconverter")
                         except Exception:
                             print(f"[ERROR] Во время создания папки SAVEconverter возникла ошибка. "
                                   f"Возможно нет прав на создания папок.\n")
-                            
+
                             response = False
-                    
+
                     print(f"    Создание файла SAVEconverter/mlp_save_prime_{ii}.xml")
-                    
+
                     try:
                         with open(file=f"SAVEconverter/mlp_save_prime_{ii}.xml",
                                   mode="w",
                                   encoding="UTF-8") as mlp_save_prime_xml:
                             mlp_save_prime_xml.write(data.decode(encoding="UTF-8",
                                                                  errors="ignore"))
-                            
+
                             ii += 1
                     except Exception:
-                        print(f"[WARNING] Во время создания файла SAVEconverter/mlp_save_prime_{ii}.xml возникла ошибка. "
-                              f"Возможно нет прав на создания файлов. "
-                              f"Файл пропущен.\n")
-                        
+                        print(
+                            f"[WARNING] Во время создания файла SAVEconverter/mlp_save_prime_{ii}.xml возникла ошибка. "
+                            f"Возможно нет прав на создания файлов. "
+                            f"Файл пропущен.\n")
+
                         response = False
             except Exception:
                 print(f"[WARNING] При обработке файла {folder}{file} возникла ошибка. "
-                              f"Возможно данные в файле повреждены или нет прав на чтение файлов. "
-                              f"Файл пропущен.\n")
-                
+                      f"Возможно данные в файле повреждены или нет прав на чтение файлов. "
+                      f"Файл пропущен.\n")
+
                 response = False
-        
+
             i += 1
-            
+
         return response
     except Exception:
         print("[ERROR] Во время обработки файлов возникла ошибка. "
               "Возможно файлы повреждены или нет прав на чтение файлов.\n")
-        
-        return False       
+
+        return False
 
 
 def load_bin_files():
     try:
         files = [x for x in argv[1:] if (isfile(path=x) and x.endswith(".bin"))]
-        
-        if (len(files) > 0):
-            return find_bin_files(keywords=keywords,
-                                  files=files,
+
+        if len(files) > 0:
+            return find_bin_files(files=files,
                                   folder="")
         else:
             if exists(path="bin"):
                 files = [x for x in listdir(path="bin") if (isfile(path=f"bin/{x}") and f"bin/{x}".endswith(".bin"))]
-                            
-                if (len(files) > 0):
-                    return find_bin_files(keywords=keywords,
-                                          files=files,
+
+                if len(files) > 0:
+                    return find_bin_files(files=files,
                                           folder="bin/")
                 else:
                     print("[ERROR] В папке bin нет файлов. "
                           "Загрузите в нее бинарные файлы в которых нужно найти данные.\n")
-                    
+
                     return False
             else:
                 print("[ERROR] Папки bin не существует. "
                       "Была создана пустая папка. "
                       "Загрузите в нее бинарные файлы в которых нужно найти данные.\n")
-                
+
                 try:
                     makedirs(name="bin")
                 except Exception:
                     print(f"[ERROR] Во время создания папки bin возникла ошибка. "
                           f"Возможно нет прав на создания папок.\n")
-                
+
                 return False
     except Exception:
         print("[ERROR] Во время обработки файлов возникла ошибка. "
               "Возможно данные в файлах повреждены или нет прав на чтение файлов.\n")
-        
+
         return False
 
 
